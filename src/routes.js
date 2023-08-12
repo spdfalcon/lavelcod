9d0123e9dbc71e163bc613f6f95b1705037f5a18
mahdi delavar <mdelever77@gmail.com>
2023-08-12 19:42:35 +0330

    layout and footer component added and header component updated !

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
-import Index from "./pages/Index/Index"
-import OurExpertises from "./pages/OurExpertises/OurExpertises"
+import Index from "./pages/Index/Home"
+import OurExpertise from "./pages/OurExpertise/OurExpertise"
 import OurProfession from "./pages/OurProfession/OurProfession"
 import Candidates from "./pages/Candidates/Candidates"
 import WhoWeAre from "./pages/WhoWeAre/WhoWeAre"
@@ -7,7 +7,7 @@ import Contact from "./pages/Contact/Contact"
 import OurOffers from "./pages/OurOffers/OurOffers"
 let routes = [
     {path:'/' , element:<Index></Index>},
-    {path:'OurExpertises' , element:<OurExpertises></OurExpertises>},
+    {path:'OurExpertise' , element:<OurExpertise></OurExpertise>},
     {path:'OurProfession' , element:<OurProfession></OurProfession>},
     {path:'Candidates' , element:<Candidates></Candidates>},
     {path:'WhoWeAre' , element:<WhoWeAre></WhoWeAre>},

6f413f2af253e86300b0385ba9b5b584f196ce75
azizollah <azizollahpayandeh1@gmail.com>
2023-08-14 00:48:28 +0430

    Navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import NavBar from '../../components/NavBar/NavBar'
 
 export default function Header() {
   return (
     <div className='w-full p-4 bg-main-blue'>
+     <NavBar/>
      <h1>header</h1>
+
     </div>
   )
 }
--- src/pages/Index/Home.jsx
+++ src/pages/Index/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Layout from '../../Layout/Layout'
-
 export default function Index() {
   return (
     <Layout>
+      
       <div className='col-span-12 text-6xl'>
         <div className='bg-main-yellow font-first-font'>main-yellow   -    first-font</div>
         <div className=' text-main-text font-second-font'>main-text -    second-font</div>

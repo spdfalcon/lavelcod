8d06789e50ddb94f1661fdf873d0997f1ee46b3b
azizollah <azizollahpayandeh1@gmail.com>
2023-08-14 23:08:44 +0430

    Navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import NavBar from '../../components/NavBar/NavBar'
 
 export default function Header() {
   return (
     <div className='w-full p-4 bg-main-blue'>
-     <NavBar/>
      <h1>header</h1>
-
     </div>
   )
 }
